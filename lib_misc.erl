dc1eefe9f06272cb7137c804e7a9324ee8fc6586
Caleb Crane <github@simulacre.org>
2012-02-26 00:28:32 +0900

    filter2 uses if statement instead of case

--- lib_misc.erl
+++ lib_misc.erl
@@ -7,12 +7,22 @@ sum([], N)   -> N;
 sum([H|T],N) -> sum(T, H+N).
 
 map(_, [])    -> [];
-map(C, [H|T]) -> [map(C, H)|map(C, T)];
-map(C, E)     -> C(E).
+map(C, [H|T]) -> [C(H)|map(C, T)].
+
 
 filter(_, []) -> [];
 filter(C, [H|T]) -> 
   case C(H) of
-    true -> [H|filter(C,T)];
+    true  -> [H|filter(C,T)];
     false -> filter(C,T)
+  end.
+
+
+filter2(_, []) -> [];
+filter2(C, [H|T]) ->
+  Val = C(H),
+  if Val  ->
+        [H|filter2(C,T)];
+     true ->
+        filter2(C,T)
   end.
