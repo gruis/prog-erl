556cd747c635317ff1b6db049e9a908addcd6896
Caleb Crane <github@simulacre.org>
2012-02-26 14:25:22 +0900

    qsort

--- lib_misc.erl
+++ lib_misc.erl
@@ -24,3 +24,21 @@ filter2(C, [H|T]) ->
      true ->
         filter2(C,T)
   end.
+
+%% Split list into Pivot and T; split T into two lists: those items
+%% that are less than Pivot and those that are greater than Pivot. 
+%% Call qsort on each list then join the lists with Pivot in the middle.
+%%
+%% Smaller = [X || X <- T, X < Pivot].
+%% Bigger  = [X || X <- T, X >= Pivot].
+%% qsort(Smaller) ++ [Pivot] ++ qsort(Bigger).
+%%
+%% L=[23,6,2,9,27,400,78,45,61,82,14].
+%% lib_misc:qsort(L).
+%% [2,6,9,14,23,27,45,61,78,82,400]
+%% qsort([6,2,9,14]) ++ [23] ++ qsort([27,400,78,45,61,82])
+qsort([]) -> [];
+qsort([Pivot|T]) ->
+  qsort([X || X <- T, X < Pivot])
+  ++ [Pivot] ++
+  qsort([X || X <- T, X >= Pivot]).
