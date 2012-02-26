853fa7c52ac0bcc78b9360036e124d262312566e
Caleb Crane <github@simulacre.org>
2012-02-26 18:54:22 +0900

    C5p93

--- shop.erl
+++ shop.erl
@@ -1,6 +1,8 @@
 -module(shop).
 -compile(export_all).
 -import(lists, [map/2, sum/1]).
+-vsn({0,0,1}).
+%%  [A || {vsn,A} <- shop:module_info(attributes)].
 
 cost(oranges)   -> 5;
 cost(newspaper) -> 8;
@@ -12,4 +14,7 @@ cost(milk)      -> 7.
 total(L) ->
   sum([cost(I) * N || {I,N} <- L]).
 
-version() -> "0.0.1".
+version() -> 
+  version([A || {vsn, A} <- module_info(attributes)]).
+version([{Maj,Min,Mic}]) -> {Maj,Min,Mic};
+version([H|_]) -> version(H).
